8e5156ab186533edb9d9278c61f631597809a03c
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-02-15 20:54:58 +0100

    refactored PersonalInfo component

---
+++ src/components/Education.jsx
@@ -0,0 +1,44 @@
+export default function EducationInfoForm({ userData, setUserData, submit, setSubmit }) {
+  function onSubmit(e) {
+    const formData = new FormData(e.target);
+    e.preventDefault();
+    setUserData({
+      ...userData,
+      school: formData.get("school"),
+      degree: formData.get("degree"),
+      startDate: formData.get("startDate"),
+      endDate: formData.get("endDate"),
+    });
+    setSubmit({ ...submit, educationInfoForm: true });
+  }
+  return (
+    <section>
+      <button type="button">Edit Education Info</button>
+      <form action="#" onSubmit={onSubmit}>
+        <label htmlFor="school">School: </label>
+        <input type="text" id="school" name="school" defaultValue={userData.school} />
+        <label htmlFor="degree">Degree: </label>
+        <input type="text" name="degree" id="degree" defaultValue={userData.degree} />
+        <label htmlFor="startDate">From: </label>
+        <input type="date" name="startDate" id="startDate" defaultValue={userData.startDate} />
+        <label htmlFor="endDate">Until: </label>
+        <input type="date" id="endDate" name="endDate" defaultValue={userData.endDate} />
+        <button type="submit">Submit</button>
+      </form>
+    </section>
+  );
+}
+
+export function EducationSection({ formData, submit }) {
+  return (
+    <>
+      {submit.educationInfoForm ? (
+        <div className="education-info">
+          <p>{formData.school}</p>
+          <p>{formData.degree}</p>
+          <p>{formData.startDate + "-" + formData.endDate}</p>
+        </div>
+      ) : null}
+    </>
+  );
+}
